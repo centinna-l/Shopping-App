3ec27812a1b20bf6c39dc46f08852981a08b4e56
centinna-l <jerryjoy99@gmail.com>
2021-02-25 15:50:47 +0530

    Added Products for testing

---
+++ lib/widgets/products_items.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProductsItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  ProductsItem({this.id, this.title, this.imageUrl});
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+              icon: Icon(
+                Icons.favorite_border_outlined,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {
+                print("Favorite Button CLicked");
+              }),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart_outlined,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {
+              print("Shopping cart button pressed");
+            },
+          ),
+          title: Text(
+            '${title}',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
