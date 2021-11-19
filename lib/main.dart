0f77de16f9bc7627860513f33501b051497cf51a
centinna-l <jerryjoy99@gmail.com>
2021-11-19 11:36:46 +0530

    Debug mode off

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,12 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'MyShop',
           theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange),
           ),
           home: ProductsOverviewScreen(),
           routes: {
